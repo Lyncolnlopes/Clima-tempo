41003c3af393d7fbde23ea26672f09b681c781ab
Lyncoln <Lyncolnnegao7@gmail.com>
2023-06-23 16:13:39 -0300

    modific background
    background sendo modificado de acordo com a temperatura do local

--- clima-tempo/script.js
+++ clima-tempo/script.js
@@ -1,5 +1,5 @@
 const apikey = "0cfd495e77ab3aa7aed3a94fa738ba4b"
-const apiCountryURL = "https://flagsapi.com/DE/flat/64/png/"
+const apiCountryURL = "https://flagsapi.com/flat/64.png/"
 const cityInput = document.querySelector("#city-input")
 const searchbtn = document.querySelector("#search")
 
@@ -7,21 +7,23 @@ const cityElement = document.querySelector('#city')
 const tempElement = document.querySelector('#temperatura span')
 const descElement = document.querySelector('#description')
 const weatherIconElement = document.querySelector('#weather-icon')
-const  countryElement = document.querySelector('#country')
+const countryElement = document.querySelector('#country')
 const umidityElement = document.querySelector('#umidity span')
 const windElement = document.querySelector('#wind span')
 
 //função
 
-const getweatherdata = async (city) =>{
+const getweatherdata = async (city) => {
 
-    
+
     const apiweatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${apikey}`
 
-    const res = await fetch (apiweatherURL)
-    const data =  await res.json()
- 
-    return data 
+    const res = await fetch(apiweatherURL)
+    const data = await res.json()
+
+
+
+    return data
 }
 
 const showWeatherData = async (city) => {
@@ -29,11 +31,30 @@ const showWeatherData = async (city) => {
 
     cityElement.innerText = data.name
     tempElement.innerText = parseInt(data.main.temp)
-    weatherIconElement.setAttribute('src',`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
-    countryElement.setAttribute('src',apiCountryURL + data.sys.country)
+    weatherIconElement.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    countryElement.setAttribute('src', apiCountryURL + data.sys.country)
     windElement.innerText = `${data.wind.speed}km/h`
     umidityElement.innerText = `${data.main.humidity}%`
     descElement.innerText = data.weather[0].description
+
+    const body = document.querySelector('body');
+    const temperature = data.main.temp;
+
+
+    if (temperature >= 30) {
+        body.style.background = 'url(https://images.pexels.com/photos/1533483/pexels-photo-1533483.jpeg?auto=compress&cs=tinysrgb&w=1700)';
+    } else if (temperature >= 20) {
+        body.style.background = 'url(https://images.pexels.com/photos/2586067/pexels-photo-2586067.jpeg?auto=compress&cs=tinysrgb&w=1700)';
+    } else if (temperature >= 10) {
+        body.style.background ='url(https://images.pexels.com/photos/1460230/pexels-photo-1460230.jpeg?auto=compress&cs=tinysrgb&w=1700)' ;
+    } else {
+        body.style.background = 'url(https://images.pexels.com/photos/6526177/pexels-photo-6526177.jpeg?auto=compress&cs=tinysrgb&w=1700)';
+    }
+
+
+
+
+
 
 }
 
@@ -42,21 +63,21 @@ const showWeatherData = async (city) => {
 
 
 
-searchbtn.addEventListener('click', async (e) => { 
+searchbtn.addEventListener('click', async (e) => {
     e.preventDefault()
-   
-    const city =cityInput.value
+
+    const city = cityInput.value
 
     showWeatherData(city)
 
-     
+
     //console.log(city)
-})   
-document.addEventListener("keypress", function(e){
-    if (e.key === 'Enter'){
+})
+document.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
         var bnt = document.querySelector("#search");
         bnt.click();
-        
+
         document.getElementById('weather-data').style.display = 'block'
     }
 })
